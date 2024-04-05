258c476642df2858e3e91c41cc6c440b4bc88443
OrigamingWasTaken <74014262+OrigamingWasTaken@users.noreply.github.com>
2024-04-05 18:12:03 +0200

    Fix imports

--- frontend/src/windows/main/pages/Settings/Panel.svelte
+++ frontend/src/windows/main/pages/Settings/Panel.svelte
@@ -54,6 +54,7 @@ $: {
 		dispatch('settingsChanged', sections);
 	}
 }
+
 </script>
 
 {#if settingsLoaded}
@@ -91,10 +92,7 @@ $: {
 						{:else if inter.options.type === 'dropdown'}
 							<Select.Root
 								items={inter.options.list}
-								selected={sections[section.id][inter.id]}
-								onSelectedChange={(e) => {
-									sections[section.id][inter.id] = e?.value
-								}}>
+								selected={sections[section.id][inter.id]}>
 								<Select.Trigger class="w-[180px] bg-gray-900 grayscale ml-auto mr-4 border-none">
 									<Select.Value placeholder={inter.options.default.label}/>
 								</Select.Trigger>
