f8a6bb27bc88da36359641f35c3724a1f9f3928a
OrigamingWasTaken <74014262+OrigamingWasTaken@users.noreply.github.com>
2024-07-31 16:34:54 +0200

    Bump version

--- build.config.ts
+++ build.config.ts
@@ -12,7 +12,13 @@ const BuildConfig: Config = {
         architecture: ["universal","arm64","x64"],
         appIcon: path("./build/assets/mac.icns"),
         minimumOS: "10.13.0"
-    }
+    },
+    // May sometimes test some things
+    // win: {
+    //     architecture: ["x64"],
+    //     appIcon: path("./frontend/src/assets/favicon.png"),
+    //     embedResources: false
+    // }
 }
 
 export default BuildConfig
