056b51845cddc25cf12a8a0728694473b460a355
OrigamingWasTaken <74014262+OrigamingWasTaken@users.noreply.github.com>
2024-10-16 14:07:11 +0200

    Only show the window once Neutralino has loaded

--- frontend/src/windows/main/main.ts
+++ frontend/src/windows/main/main.ts
@@ -5,11 +5,12 @@ import './ts/roblox';
 import './ts/window';
 
 // Imports
-import { events, init, app as neuApp } from '@neutralinojs/lib';
+import { events, init, app as neuApp, window as neuWindow } from '@neutralinojs/lib';
 import { version } from '../../../../package.json';
 import App from './App.svelte';
 import { RPCController } from './ts/tools/rpc';
 import { shell } from './ts/tools/shell';
+import { focusWindow } from './ts/window';
 
 // Initialize NeutralinoJS
 init();
@@ -17,11 +18,14 @@ init();
 async function quit() {
 	console.info('[Main] Exiting app');
 	await RPCController.stop();
+	await shell('pkill', ['-f', '_ablox'], { skipStderrCheck: true });
 	await neuApp.exit();
 }
 
 // When NeutralinoJS is ready:
 events.on('ready', async () => {
+	neuWindow.show()
+	focusWindow()
 	setTimeout(async () => {
 		console.info(`[Main] AppleBlox v${version}`);
 		console.info(`[Main] Current Time: ${new Date().toLocaleString()}`);
