ee08f8ad8376e5365a2addc041f3b55f4573cd86
OrigamingWasTaken <74014262+OrigamingWasTaken@users.noreply.github.com>
2024-10-18 01:06:52 +0200

    Overhaul Informations tab (better credits)

---
+++ frontend/src/windows/main/components/informations/informations.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+	import Accordion from './accordion.svelte';
+	import ContributorsCollection from './contributors-collection.svelte';
+	import type { Contributor, Technology } from './types';
+
+	const developers: Contributor[] = [
+		{
+			name: 'OrigamingWasTaken',
+			description: 'Maintainer & main developer',
+			avatar: 'https://avatars.githubusercontent.com/u/74014262?v=4',
+			link: 'https://github.com/OrigamingWasTaken',
+		},
+	];
+	const artists: Contributor[] = [
+		{ name: 'typeofnull', description: 'Made the app logo', avatar: 'https://i.imgur.com/caBlXK5.png', link: null },
+		{
+			name: 'abyzal.',
+			description: 'Made the v1 of the app logo',
+			avatar: 'https://avatars.githubusercontent.com/u/157009854?v=4',
+			link: 'https://github.com/PurvisiLOL',
+		},
+	];
+	const testers: Contributor[] = [
+		{
+			name: 'allFiction',
+			description: 'Moderator & main tester',
+			avatar: 'https://avatars.githubusercontent.com/u/143362759?v=4',
+			link: 'https://github.com/AngrySausage',
+		},
+		{
+			name: 'atomic',
+			description: 'Tester & ori yap support 😉',
+			avatar: 'https://avatars.githubusercontent.com/u/146490689?v=4',
+			link: 'https://github.com/20ch',
+		},
+	];
+
+	const technologies: Technology[] = [
+		{
+			name: 'NeutralinoJS',
+			description: 'Lightweight C++ and cross-platform framework for making apps',
+			avatar: 'https://raw.githubusercontent.com/neutralinojs/design-guide/d3a8fc284d9fff591b173d3bf53998ee0424aeea/logo/neutralinojs_logo_vector.svg',
+			link: 'https://neutralino.js.org',
+		},
+		{
+			name: 'Svelte',
+			description: 'Free and open-source component-based front-end software framework',
+			avatar: 'https://github.com/sveltejs/branding/blob/master/svelte-logo.png?raw=true',
+			link: 'https://svelte.dev',
+		},
+		{
+			name: 'TypeScript',
+			description: 'Superset of JavaScript with static types that compiles to clean JavaScript output',
+			avatar: 'https://www.typescriptlang.org/images/branding/logo-grouping.svg',
+			link: 'https://www.typescriptlang.org/',
+		},
+		{
+			name: 'Icons8',
+			description: 'Library of high quality free icons',
+			avatar: 'https://s3-eu-west-1.amazonaws.com/tpd/logos/59ca1db50000ff0005ac11cf/0x0.png',
+			link: 'https://icons8.com/',
+		},
+	];
+
+	const inspirations: Contributor[] = [
+		{
+			name: 'Bloxstrap',
+			description: 'An alternative replacement for the standard Roblox launcher, main inspiration for this app.',
+			avatar: 'https://github.com/bloxstraplabs/bloxstrap/blob/main/Images/Bloxstrap.png?raw=true',
+			link: 'https://github.com/bloxstraplabs/bloxstrap',
+		},
+	];
+</script>
+
+<div class="flex flex-col gap-2">
+	<h2 class="font-extralight text-xl text-rose-500/80">Developers</h2>
+	<ContributorsCollection contributors={developers} />
+	<h2 class="font-serif tracking-widest text-xl text-rose-400/80">Artists</h2>
+	<ContributorsCollection contributors={artists} />
+	<Accordion contributors={testers} label="Testers" class="font-mono text-xl text-rose-300/80" />
+	<Accordion contributors={technologies} label="Technologies" class="font-mono text-xl text-sky-600" />
+	<Accordion contributors={inspirations} label="Inspirations" class="font-mono text-xl text-purple-600/50" />
+</div>
--- frontend/src/windows/main/pages/Info.svelte
+++ frontend/src/windows/main/pages/Info.svelte
@@ -3,6 +3,7 @@
 	import { Github, MessageSquare } from 'lucide-svelte';
 	import { SettingsPanelBuilder } from '../components/settings';
 	import Panel from '../components/settings/panel.svelte';
+	import Informations from '../components/informations/informations.svelte';
 
 	export let render = true;
 
@@ -18,72 +19,41 @@
 		}
 	}
 
-	const devlist = `
-	<a href="https://github.com/OrigamingWasTaken">@OrigamingWasTaken</a> - Main Developper, currently the only one building AppleBlox.
-	`;
-
-	const artistlist = `
-	<a href="_blank">@typeofnull</a> - Made the current app icon.<br>
-	<a href="https://github.com/PurvisiLOL">@abyzal.</a> - Made the first version of the app icon.
-	`;
-
-	const testerlist = `
-	<a href="_blank">@allFiction</a> - People who have tested the app, and repported bugs.
-	`;
-
 	const panel = new SettingsPanelBuilder()
-		.setName('Info')
-		.setDescription('Useful resources, people that helped making the app and technologies used')
+		.setName('Informations')
 		.setId('info')
-		.addCategory((category) =>
+		.setDescription('Useful resources & awesome contributors')
+		.addCategory(category => 
 			category
-				.setName('Useful resources & support')
-				.setDescription('Links and guides for AppleBlox')
-				.setId('resources')
+				.setId("contributors")
+				.addCustom({
+					label: "",
+					description: "",
+					id: "info_page",
+					component: Informations,
+				})
+		)
+		.addCategory(category =>
+			category
+				.setName("Resources")
+				.setDescription("Useful links and guides for the app")
+				.setId("resources")
 				.addButton({
 					label: 'Join Discord server',
 					description: 'Opens the Discord server invitation link',
 					id: 'discord_btn',
-					variant: 'default',
+					variant: 'outline',
 					icon: { component: MessageSquare },
 				})
 				.addButton({
 					label: 'Github Repo',
 					description: 'Opens the Github repository',
 					id: 'github_btn',
-					variant: 'secondary',
+					variant: 'outline',
 					icon: { component: Github },
 				})
 		)
-		.addCategory((category) =>
-			category
-				.setName('Contributors')
-				.setDescription('The ones who make AppleBlox')
-				.setId('contributors')
-				.addEmpty({ label: 'Developers', description: devlist, id: 'developers' })
-				.addEmpty({ label: 'Artists', description: artistlist, id: 'artists' })
-				.addEmpty({ label: 'Testers', description: testerlist, id: 'testers' })
-		)
-		.addCategory((category) =>
-			category
-				.setName('Technologies')
-				.setDescription('Technologies used in AppleBlox')
-				.setId('technologies')
-				.addEmpty({ label: 'Icons8 - https://icons8.com', description: 'A lot of icons in the app', id: 'icons8' })
-		)
-		.addCategory((category) =>
-			category
-				.setName('Inspirations')
-				.setDescription('People or projects that gave ideas for AppleBlox')
-				.setId('inspirations')
-				.addEmpty({
-					label: 'Bloxstrap - https://github.com/pizzaboxer/bloxstrap',
-					description:
-						'A Roblox launcher for Windows. This was the main inspiration for this app. Made by <a href="https://github.com/pizzaboxer">@pizzaboxer</a>',
-					id: 'bloxstrap',
-				})
-		)
-		.build();
+		.build()
 </script>
 
 <Panel {panel} on:button={onButtonClicked} {render} />
