1db421f7599d2c01492110adc9a295958ed57681
OrigamingWasTaken <74014262+OrigamingWasTaken@users.noreply.github.com>
2024-10-27 12:49:33 +0100

    Various UI fixes

--- frontend/src/windows/main/components/color-image.svelte
+++ frontend/src/windows/main/components/color-image.svelte
@@ -3,16 +3,25 @@
 
 	export let src: string;
 	export let alt: string;
-	export let color = '';
+	export let color = '#000000'; // Default to black if no color provided
 
 	let className = '';
 	export { className as class };
 </script>
 
 <div class={cn('relative', className)}>
-	<img {src} {alt} class="w-full h-full object-contain" />
+	<img 
+		{src} 
+		{alt} 
+		class="w-full h-full object-contain"
+		style="visibility: hidden;"
+	/>
 	<div
-		class={`absolute inset-0 bg-${color}`}
-		style="mask-image: url({src}); mask-size: contain; mask-repeat: no-repeat; mask-position: center;"
-	></div>
+		class="absolute inset-0"
+		style:mask-image={`url(${src})`}
+		style:mask-size="contain"
+		style:mask-repeat="no-repeat"
+		style:mask-position="center"
+		style:background-color={color}
+	/>
 </div>
