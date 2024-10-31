74dd6612eaeaba912b493a9f90637451194bac79
OrigamingWasTaken <contact@origaming.ch>
2024-10-31 21:27:23 +0100

    Re-add command component

---
+++ frontend/src/lib/components/ui/command/command-shortcut.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { cn } from '$lib/utils.js';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	type $$Props = HTMLAttributes<HTMLSpanElement>;
+
+	let className: string | undefined | null = undefined;
+	export { className as class };
+</script>
+
+<span class={cn('text-muted-foreground ml-auto text-xs tracking-widest', className)} {...$$restProps}>
+	<slot />
+</span>
