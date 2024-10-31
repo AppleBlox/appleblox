eb913e5a940ea9b2ba20eb6b971d178fdc697f4f
OrigamingWasTaken <contact@origaming.ch>
2024-10-31 21:16:20 +0100

    Add notification when closing Desktop app from "Disable Desktop app" setting

--- frontend/src/windows/main/ts/roblox/events/returnToLuaApp.ts
+++ frontend/src/windows/main/ts/roblox/events/returnToLuaApp.ts
@@ -1,5 +1,6 @@
 import { getValue } from '@/windows/main/components/settings';
 import { events } from '@neutralinojs/lib';
+import { Notification } from '../../tools/notifications';
 
 // Debounce
 let latestReturnEvent: null | number = null;
@@ -9,6 +10,7 @@ async function returnToLuaApp() {
 	latestReturnEvent = Date.now();
 	const isDesktopAppDisabled = (await getValue<boolean>('roblox.behavior.disable_desktop_app')) === true;
 	if (isDesktopAppDisabled) {
+		new Notification({ title: 'Closing desktop app', content: 'Disable desktop app setting is enabled.', timeout: 3000 }).show();
 		console.info('[Activity] Disabling desktop app by terminating.');
 		await events.broadcast('instance:quit');
 	}
