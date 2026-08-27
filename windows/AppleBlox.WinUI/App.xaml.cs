5e0f2c25745ac75be939ec7b16a3fc853ff3e55b
AgentXCO2 <thelittlestagent@gmail.com>
2026-08-26 19:51:45 -0500

    Add WinUI startup code

---
+++ windows/AppleBlox.WinUI/App.xaml.cs
@@ -0,0 +1,19 @@
+using Microsoft.UI.Xaml;
+
+namespace AppleBlox.WinUI;
+
+public partial class App : Application
+{
+    public static Window? MainWindow { get; private set; }
+
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        MainWindow = new MainWindow();
+        MainWindow.Activate();
+    }
+}
